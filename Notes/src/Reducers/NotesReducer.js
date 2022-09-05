ce9e10fa644a70a5d48956dfc738edfabd94e736
HenriqueACF <henrique.assis145@gmail.com>
2022-09-05 15:33:52 -0300

    feat: add redux

---
+++ Notes/src/Reducers/NotesReducer.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+  list:[
+    {title:'Note Test', body:'Testando'}
+  ]
+}
+
+export default (state = initialState, action) =>{
+  switch(action.type){
+    case 'ADD_NOTE':
+      break
+  }
+  return state
+}
